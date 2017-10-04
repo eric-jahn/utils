db79dc77ba603e941bcd8422bfa3fb64da2faa50
Eric Jahn <eric@ejahn.net>
2017-10-03 23:19:02 -0400

    handling __

--- snake-case/snake_case.py
+++ snake-case/snake_case.py
@@ -3,14 +3,15 @@ import sys
 import re
 
 def snakify(filename):
+    filename = filename.replace('__', '_')
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', filename)
+    re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
     s1.replace('__', '_')
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
-
+    return s1
 fn = sys.argv[1]
 if os.path.exists(fn):
     print "path exists: " + fn
     for filename in os.listdir(fn):
         renamed = snakify(filename)
         os.rename(fn + "/" + filename, os.path.join(fn, renamed))
-        print "renaming " + filename + "->" + renamed
+        print "renaming " + filename + " -> " + renamed
